58ed57968fb54eb57cc80bab44f481970f692e63
mohand804 <mohandabstar@gmail.com>
2025-10-19 16:39:18 +0300

    Add basic Flutter CI workflow

--- lib/features/deatalis_screen/ui/deatails_screen.dart
+++ lib/features/deatalis_screen/ui/deatails_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:week2/core/theming/colors_manger.dart';
@@ -16,10 +18,61 @@ class _AnimeDetailsScreenState extends State<AnimeDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand,
         children: [
-          Image.asset("assets/images/Rectangle.png", fit: BoxFit.cover),
-          ImagesAndText(),
+          Container(color: ColorsManager.deepviolet),
+          Positioned(
+            bottom: -80.h,
+            left: -20.w,
+            child: Container(
+              width: 120.w,
+              height: 120.h,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  colors: [
+                    Color(0xFF713649).withOpacity(0.20),
+                    Color(0xFF713649).withOpacity(0.12),
+                    Color(0xFF713649).withOpacity(0.03),
+                  ],
+                ),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF713649).withOpacity(0.15),
+                    blurRadius: 15.r,
+                    spreadRadius: 3.r,
+                    offset: Offset(0, 3.h),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 130.h,
+            right: -50.w,
+            child: Container(
+              width: 150.w,
+              height: 150.h,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  colors: [
+                    Color(0xFF713649).withOpacity(0.30),
+                    Color(0xFF713649).withOpacity(0.05),
+                    Color(0xFF713649).withOpacity(0.06),
+                  ],
+                ),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF713649).withOpacity(0.3),
+                    blurRadius: 18.r,
+                    spreadRadius: 4.r,
+                    offset: Offset(0, 4.h),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SingleChildScrollView(child: Column(children: [ImagesAndText()])),
         ],
       ),
       bottomNavigationBar: Container(
